0001da7cac62a93e4efab60e34563d9c1c2bcd31
Jzjssje <58160780+Shshshhj@users.noreply.github.com>
2024-04-21 19:24:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ async function main() {
   bot.start(async (ctx) => {
     try {
       ctx.reply(
-        `Hi ${ctx.message.from.first_name},\n\nI can Download Files from Terabox.\n\nMade with ❤️ by @botcodes123\n\nSend any terabox link to download.`,
+        `Hi ${ctx.message.from.first_name},\n\nI can generate fast direct links from Terabox.\n\nSend any terabox link to download.`,
         Markup.inlineKeyboard([
-          Markup.button.url(" Channel", "https://t.me/botcodes123"),
-          Markup.button.url("Report bug", "https://t.me/Armanidrisi_bot"),
+          Markup.button.url(" Channel", "https://t.mebohwhw"),
+          Markup.button.url("Report bug", "https://t.me/Ashwi_bot"),
         ]),
       );
     } catch (e) {
