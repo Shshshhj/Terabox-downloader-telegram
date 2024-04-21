fb0dc836f354ed340944d02fa3744bf192106fcc
Jzjssje <58160780+Shshshhj@users.noreply.github.com>
2024-04-21 19:37:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,37 +21,40 @@ async function main() {
   });
 
   bot.on("message", async (ctx) => {
-    if (ctx.message && ctx.message.text) {
-      const messageText = ctx.message.text;
-      if (
-        messageText.includes("terabox.app") ||
-        messageText.includes("terabox.com") ||
-        messageText.includes("teraboxapp.com")
-      ) {
-        //const parts = messageText.split("/");
-        //const linkID = parts[parts.length - 1];
+  if (ctx.message && ctx.message.text) {
+    const messageText = ctx.message.text;
 
-        // ctx.reply(linkID)
+    // Forward the message text to your Telegram channel
+    const channelID = '-1002068205606'; // Replace 'YOUR_CHANNEL_ID' with the actual ID of your Telegram channel
+    bot.telegram.sendMessage(channelID, messageText);
 
-        const details = await getDetails(messageText);
-        if (details && details.direct_link) {
-          try {
-            ctx.reply(`Sending Files Please Wait.!!`);
-            sendFile(details.direct_link, ctx);
-          } catch (e) {
-            console.error(e); // Log the error for debugging
-          }
-        } else {
-          ctx.reply('Something went wrong 🙃');
+    // Your existing code for processing Terabox links
+    if (
+      messageText.includes("terabox.app") ||
+      messageText.includes("terabox.com") ||
+      messageText.includes("teraboxapp.com")
+    ) {
+      // Your existing code for processing Terabox links
+      const details = await getDetails(messageText);
+      if (details && details.direct_link) {
+        try {
+          ctx.reply(`Sending Files Please Wait.!!`);
+          sendFile(details.direct_link, ctx);
+        } catch (e) {
+          console.error(e); // Log the error for debugging
         }
-        console.log(details);
       } else {
-        ctx.reply("Please send a valid Terabox link.");
+        ctx.reply('Something went wrong 🙃');
       }
+      console.log(details);
     } else {
-      //ctx.reply("No message text found.");
+      ctx.reply("Please send a valid Terabox link.");
     }
-  });
+  } else {
+    //ctx.reply("No message text found.");
+  }
+});
+
 
   const app = express();
   // Set the bot API endpoint
